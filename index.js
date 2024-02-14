dae42e072b9969d5a98f02464867f81b5dac6b85
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-14 21:51:22 +0530

    create function to generate unique random number

--- index.js
+++ index.js
@@ -9,11 +9,31 @@ let decrease = document.getElementById("decrease");
 let reset = document.getElementById("reset");
 let increase = document.getElementById("increase");
 let count = 0;
+let randomIndex = 0;
+let tempRandom;
 
 // Creating an array to store colours
 
 let colours = ['#92FFF2', '#FF92D3', '#9B92FF', '#E9FF92', '#FD974E', 
               '#4EFDA9', '#FD4E4E', '#FFD700', '#FD4EEB', '#0085FF',]
+
+/*Creating a function that generates a random number and use it as index 
+in colours array to change the background color of the page and + symbol*/
+
+function changeColor() {
+
+    tempRandom = Math.floor(Math.random() * colours.length);
+
+    if (tempRandom === randomIndex){
+        tempRandom = Math.floor(Math.random() * colours.length);
+    }
+    else {
+        randomIndex = tempRandom;
+    }
+
+}
+
+
 
 
 // arrow function expression to decrease the count
